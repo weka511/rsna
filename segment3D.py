eed37a76606c01a9a297470a31618407ef8d3c15
Simon Crase <simon@greenweaves.nz>
2021-07-31 07:56:44 +1200

    Added histograms

--- segment3D.py
+++ segment3D.py
@@ -21,9 +21,11 @@
 # SOFTWARE.
 
 from argparse          import ArgumentParser
+from matplotlib.cm     import viridis
 from matplotlib.pyplot import figure, savefig, show
 from mri3d             import Labelled_MRI_Dataset, MRI_Geometry, Study
 from numpy             import array, matmul, mean, std
+from os.path           import join
 
 def get_3d(series):
     xs = []
@@ -31,17 +33,16 @@ def get_3d(series):
     zs = []
     cs = []
     for dcim in series.dcmread():
-        if dcim.pixel_array.sum()>0:
-            Matrix = MRI_Geometry.create_matrix(dcim)
-            for i in range(0,dcim.Rows,args.stride):
-                for j in range(0,dcim.Columns,args.stride):
-                    if dcim.pixel_array[i,j]>0:
-                        vector_ij = array([i,j,0,1])
-                        vector_XYZ = matmul(Matrix,vector_ij)
-                        xs.append(vector_XYZ[0])
-                        ys.append(vector_XYZ[1])
-                        zs.append(vector_XYZ[2])
-                        cs.append(dcim.pixel_array[i,j])
+        Matrix = MRI_Geometry.create_matrix(dcim)
+        for i in range(0,dcim.Rows,args.stride):
+            for j in range(0,dcim.Columns,args.stride):
+                if dcim.pixel_array[i,j]>0:
+                    vector_ij  = array([i,j,0,1])
+                    vector_XYZ = matmul(Matrix,vector_ij)
+                    xs.append(vector_XYZ[0])
+                    ys.append(vector_XYZ[1])
+                    zs.append(vector_XYZ[2])
+                    cs.append(dcim.pixel_array[i,j])
     return xs,ys,zs,cs
 
 def  extract(xs0,ys0,zs0,cs0,n=0.7):
@@ -61,37 +62,48 @@ def  extract(xs0,ys0,zs0,cs0,n=0.7):
 
 if __name__=='__main__':
     parser = ArgumentParser('Visualize & segment in 3D')
-    parser.add_argument('--path',     default = r'D:\data\rsna',              help = 'Path for data')
-    parser.add_argument('--figs',     default = './figs',                     help = 'Path to store plots')
-    parser.add_argument('--show',     default = False, action = 'store_true', help = 'Set if plots are to be displayed')
-    parser.add_argument('--study',    default = '00098',                      help = 'Name of Studies to be processed' )
-    parser.add_argument('--cmap',     default = 'gray',                       help = 'Colour map for displaying greyscale images')
-    parser.add_argument('--stride',   default = 1, type = int)
-    args = parser.parse_args()
-
-    dataset = Labelled_MRI_Dataset(args.path,'train')
-    study   = dataset[args.study]
-    label   = dataset.get_label(args.study)
-    Threshold = {'FLAIR' : 0.7,
-                 'T1wCE' : 0.9,
-                 'T2w'   : 0.7}
+    parser.add_argument('--path',       default = r'D:\data\rsna',              help = 'Path for data')
+    parser.add_argument('--figs',       default = './figs',                     help = 'Path to store plots')
+    parser.add_argument('--show',       default = False, action = 'store_true', help = 'Set if plots are to be displayed')
+    parser.add_argument('--study',      default = '00098',                      help = 'Name of Studies to be processed' )
+    parser.add_argument('--cmap',       default = 'gray',                       help = 'Colour map for displaying greyscale images')
+    parser.add_argument('--stride',     default = 1, type = int,                help = 'Stride for 2D image plane')
+    parser.add_argument('--thresholds', default = ['FLAIR', 0.7, 'T1wCE', 0.9, 'T2w', 0.7], nargs='*', help = 'Cutoff values for plot')
+    parser.add_argument('--hist',       default = False, action = 'store_true', help = 'Set if histograms are to be displayed')
+    args       = parser.parse_args()
+    dataset    = Labelled_MRI_Dataset(args.path,'train')
+    study      = dataset[args.study]
+    label      = dataset.get_label(args.study)
+    Threshold  = {args.thresholds[i]:float(args.thresholds[i+1]) for i in range(0,len(args.thresholds),2)}
     for series in study.get_series():
         fig = figure(figsize=(20,20))
-        ax = fig.add_subplot(2,1,1,projection='3d')
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
-        ax.set_title(series.description)
+        ax1 = fig.add_subplot(2,2,1,projection='3d')
+        ax1.set_xlabel('X')
+        ax1.set_ylabel('Y')
+        ax1.set_zlabel('Z')
+        ax1.set_title(series.description)
         xs,ys,zs,cs = get_3d(series)
-        ax.scatter(xs,ys,zs,c=cs,s=1)
+        vmin        = min(cs)
+        vmax        = max(cs)
+        mu          = mean(cs)
+        sigma       = std(cs)
+        im1         = ax1.scatter(xs,ys,zs,c=cs,s=1,cmap=viridis,vmin=vmin,vmax=vmax)
+        fig.colorbar(im1, ax=ax1)
+        if args.hist:
+            ax2 = fig.add_subplot(2,2,2)
+            ax2.hist(cs)
+            ax2.set_title(f'Mean = {mu:.0f}, std={sigma:.0f}')
         if series.description in Threshold:
             xs,ys,zs,cs = extract(xs,ys,zs,cs,n=Threshold[series.description])
-            ax = fig.add_subplot(2,1,2,projection='3d')
-            ax.set_xlabel('X')
-            ax.set_ylabel('Y')
-            ax.set_zlabel('Z')
-            ax.scatter(xs,ys,zs,c=cs,s=1)
-    fig.suptitle(f'{study.name} {label}')
+            ax3 = fig.add_subplot(2,2,3,projection='3d')
+            ax3.set_xlabel('X')
+            ax3.set_ylabel('Y')
+            ax3.set_zlabel('Z')
+            im3 = ax3.scatter(xs,ys,zs,c=cs,s=1,cmap=viridis,vmin=vmin,vmax=vmax)
+            fig.colorbar(im3, ax=ax3)
+            ax3.set_title(f'Values over {mu:.0f} + {Threshold[series.description]} * {sigma:.0f} = {mu+Threshold[series.description]*sigma:.0f}')
+        fig.suptitle(f'{study.name} {label} {series.description}')
+        savefig(join(args.figs,f'{args.study}-{series.description}-3D'))
     if args.show:
         show()
 
