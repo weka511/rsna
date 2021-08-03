4b3d32f76a5956c8e0ed8336e81d4d50e96cf0a6
Simon Crase <simon@greenweaves.nz>
2021-08-03 16:27:19 +1200

    #6 refactored

--- reslice.py
+++ reslice.py
@@ -1,84 +1,56 @@
-"""
-==========================================================
-Load CT slices and plot axial, sagittal and coronal images
-==========================================================
 
-This example illustrates loading multiple files, sorting them by slice
-location, building a 3D image and reslicing it in different planes.
-
-.. usage:
-
-   reslice.py <glob>
-   where <glob> refers to a set of DICOM image files.
-
-   Example: python reslice.py "*.dcm". The quotes are needed to protect
-   the glob from your system and leave it for the script.
-
-.. note:
-
-   Uses numpy and matplotlib.
-
-   Tested using series 2 from here
-   http://www.pcir.org/researchers/54879843_20060101.html
-"""
-
-import pydicom
-import numpy as np
-import matplotlib.pyplot as plt
-import sys
-import glob
+from pydicom           import dcmread
+from numpy             import zeros
+from matplotlib.pyplot import subplot, imshow, show
+from sys               import argv
+from glob              import glob
 
 # load the DICOM files
-files = []
-print('glob: {}'.format(sys.argv[1]))
-for fname in glob.glob(sys.argv[1], recursive=False):
-    print("loading: {}".format(fname))
-    files.append(pydicom.dcmread(fname))
 
-print("file count: {}".format(len(files)))
+def read_slices(paths):
+    files = [dcmread(fname) for fname in paths]
 
-# skip files with no SliceLocation (eg scout views)
-slices = []
-skipcount = 0
-for f in files:
-    if hasattr(f, 'SliceLocation'):
-        slices.append(f)
-    else:
-        skipcount = skipcount + 1
+    print("file count: {}".format(len(files)))
 
-print("skipped, no SliceLocation: {}".format(skipcount))
+    return sorted([f for f in files if hasattr(f, 'SliceLocation')],
+                    key=lambda s: s.SliceLocation)
 
-# ensure they are in the correct order
-slices = sorted(slices, key=lambda s: s.SliceLocation)
+def get_pixel_aspects(slices):
+    # pixel aspects, assuming all slices are the same
+    ps         = slices[0].PixelSpacing
+    ss         = slices[0].SliceThickness
+    return ps[1]/ps[0], ps[1]/ss, ss/ps[0]
 
-# pixel aspects, assuming all slices are the same
-ps = slices[0].PixelSpacing
-ss = slices[0].SliceThickness
-ax_aspect = ps[1]/ps[0]
-sag_aspect = ps[1]/ss
-cor_aspect = ss/ps[0]
+def create_image(slices):
+    # create 3D array
+    img_shape = list(slices[0].pixel_array.shape)
+    img_shape.append(len(slices))
+    img3d = zeros(img_shape)
 
-# create 3D array
-img_shape = list(slices[0].pixel_array.shape)
-img_shape.append(len(slices))
-img3d = np.zeros(img_shape)
+    # fill 3D array with the images from the files
+    for i, slice in enumerate(slices):
+        img2d          = slice.pixel_array
+        img3d[:, :, i] = img2d
+    return img3d, img_shape
 
-# fill 3D array with the images from the files
-for i, s in enumerate(slices):
-    img2d = s.pixel_array
-    img3d[:, :, i] = img2d
+if __name__=='__main__':
 
-# plot 3 orthogonal slices
-a1 = plt.subplot(2, 2, 1)
-plt.imshow(img3d[:, :, img_shape[2]//2])
-a1.set_aspect(ax_aspect)
+    slices = read_slices(glob(argv[1], recursive=False))
 
-a2 = plt.subplot(2, 2, 2)
-plt.imshow(img3d[:, img_shape[1]//2, :])
-a2.set_aspect(sag_aspect)
+    ax_aspect, sag_aspect, cor_aspect = get_pixel_aspects(slices)
 
-a3 = plt.subplot(2, 2, 3)
-plt.imshow(img3d[img_shape[0]//2, :, :].T)
-a3.set_aspect(cor_aspect)
+    img3d, img_shape = create_image(slices)
+    # plot 3 orthogonal slices
+    a1 = subplot(2, 2, 1)
+    imshow(img3d[:, :, img_shape[2]//2])
+    a1.set_aspect(ax_aspect)
 
-plt.show()
+    a2 = subplot(2, 2, 2)
+    imshow(img3d[:, img_shape[1]//2, :])
+    a2.set_aspect(sag_aspect)
+
+    a3 = subplot(2, 2, 3)
+    imshow(img3d[img_shape[0]//2, :, :].T)
+    a3.set_aspect(cor_aspect)
+
+    show()
